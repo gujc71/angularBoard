a9c9c2af1eca09a0abe0f3bb108e7de4de505095
gujc71 <gujc71@gmail.com>
2018-02-26 23:16:00 +0900

    First Commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule }   from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { BoardComponent } from './board/board.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    BoardComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
